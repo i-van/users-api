9f5e21da8b0256121253466450f9ec2d2524aa87
Ivan Nosov <i.k.nosov@gmail.com>
2014-07-31 11:33:00 +0300

    fix validation

--- forms/user/create.js
+++ forms/user/create.js
@@ -1,48 +1,36 @@
 
 var Validation = require('validation')
-  , validators = require('validation/validators')
-  , util = require('util');
+  , validators = require('validation/validators');
 
-function UserCreateValidation() {
-    return UserCreateValidation.super_.apply(this, arguments)
-}
-
-util.inherits(UserCreateValidation, Validation);
-
-UserCreateValidation.prototype.initialize = function() {
-    this._firstName();
-    this._lastName();
-    this._email();
-    this._login();
-    this._password();
-};
-
-UserCreateValidation.prototype._firstName = function() {
-    this.add('firstName', validators.notEmpty, 'First name is required')
-};
-
-UserCreateValidation.prototype._lastName = function() {
-    this.add('lastName', validators.notEmpty, 'Last name is required')
-};
-
-UserCreateValidation.prototype._email = function() {
-    this.add('email', validators.notEmpty, 'Email is required')
-        .add('email', validators.isEmail, 'Email is not correct')
-};
-
-UserCreateValidation.prototype._login = function() {
-    this.add('login', validators.notEmpty, 'Login is required');
-    this.add(
-        'login',
-        validators.noRecordExists('User', { login: this.values.login }),
-        'Such User has been already registered'
-    )
-};
-
-UserCreateValidation.prototype._password = function() {
-    this.add('password', validators.notEmpty, 'Password is required')
-        .add('password', validators.len(6), 'Password length should be greater than 6')
-        .add('password', validators.equalField('passwordConfirmation'), 'Passwords should be matched')
-};
-
-module.exports = UserCreateValidation;
+module.exports = Validation.extend({
+    init: function() {
+        this._firstName();
+        this._lastName();
+        this._email();
+        this._login();
+        this._password();
+    },
+    _firstName: function() {
+        this.add('firstName', validators.notEmpty, 'First name is required');
+    },
+    _lastName: function() {
+        this.add('lastName', validators.notEmpty, 'Last name is required');
+    },
+    _email: function() {
+        this.add('email', validators.notEmpty, 'Email is required', true)
+            .add('email', validators.isEmail, 'Email is not correct');
+    },
+    _login: function() {
+        this.add('login', validators.notEmpty, 'Login is required', true);
+        this.add(
+            'login',
+            validators.noRecordExists('User', { login: this.values.login }),
+            'Such User has been already registered'
+        );
+    },
+    _password: function() {
+        this.add('password', validators.notEmpty, 'Password is required', true)
+            .add('password', validators.length(6), 'Password length should be greater than 6', true)
+            .add('password', validators.equalField('passwordConfirmation'), 'Passwords should be matched');
+    }
+});
--- forms/user/edit.js
+++ forms/user/edit.js
@@ -1,27 +1,19 @@
 
 var validators = require('validation/validators')
-  , UserCreateValidation = require('./create')
-  , util = require('util');
+  , UserCreateValidation = require('./create');
 
-function UserEditValidation() {
-    return UserEditValidation.super_.apply(this, arguments)
-}
-
-util.inherits(UserEditValidation, UserCreateValidation);
-
-UserEditValidation.prototype._password = function() {
-    if (this.values.password) {
-        UserEditValidation.super_.prototype._password.call(this)
+module.exports = UserCreateValidation.extend({
+    _password: function() {
+        if (this.values.password) {
+            UserCreateValidation.prototype._password.call(this);
+        }
+    },
+    _login: function() {
+        this.add('login', validators.notEmpty, 'Login is required', true);
+        this.add(
+            'login',
+            validators.noRecordExists('User', { login: this.values.login, _id: { $ne: this.values._id } }),
+            'Such User has been already registered'
+        )
     }
-};
-
-UserEditValidation.prototype._login = function() {
-    this.add('login', validators.notEmpty, 'Login is required');
-    this.add(
-        'login',
-        validators.noRecordExists('User', { login: this.values.login, _id: { $ne: this.values._id } }),
-        'Such User has been already registered'
-    )
-};
-
-module.exports = UserEditValidation;
+});
